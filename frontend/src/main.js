0dab4b81d27e59b1028dab3adc0f3471daf6429d
halidawang2-creat <halidawang2@gmail.com>
2026-09-02 11:19:10 +0800

    chore: test git push

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,5 @@ import App from "./App.vue"
 import "./style.css"
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
+// Git push connectivity test comment.
 createApp(App).use(ElementPlus).mount("#app")
